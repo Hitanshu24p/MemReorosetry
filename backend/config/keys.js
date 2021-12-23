ba7242455aece589ea6848ebde2decd5a8f9e77d
Hitanshu24p <83834384+Hitanshu24p@users.noreply.github.com>
2021-12-23 11:26:33 +0530

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
+    //mongoURI: "mongodb+srv://db_admin: Arya@2653@memcluster.a4pe5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     secretOrKey: "secret"
   };
